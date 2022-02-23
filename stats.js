6d17f1cc355c153220d9f95dabde3762f34f222f
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-02-23 12:49:43 -0500

    x

--- stats.js
+++ stats.js
@@ -8,6 +8,8 @@
   const outputPictures = document.querySelector('.outputPictures');
   const outputPortals = document.querySelector('.outputPortals');
   const outputScreenshots = document.querySelector('.outputScreenshots');
+  const outputWallets = document.querySelector('.outputWallets');
+
 
 
   circSup.innerHTML = "";
@@ -19,6 +21,7 @@
   outputPictures.innerHTML = "";
   outputPortals.innerHTML = "";
   outputScreenshots.innerHTML = "";
+  outputWallets.innerHTML = "";
 
 
   fetch(urlStats).then(function (res) {
@@ -33,12 +36,12 @@
     issuedSup.innerHTML += '<br><b>' + is.toLocaleString("en-US") + ' </b><br>';
     claimable.innerHTML += '<br><b>' + parseInt(cl).toLocaleString("en-US") + ' </b><br>';
     outputTransactionTypes.innerHTML += '<br><b>' + tt.toLocaleString("en-US") + ' </b><br>';
-    //console.log("Crystalize = " + data.data.payload.transactions.byType[4].numTransactions);
-    outputCrystallizeNFT.innerHTML += '<br><b>' + data.data.payload.transactions.byType[4].numTransactions + ' </b><br>';
-    outputMeltNFT.innerHTML += '<br><b>' + data.data.payload.transactions.byType[5].numTransactions + ' </b><br>';
-    outputPictures.innerHTML += '<br><b>' + data.data.payload.transactions.byType[6].numTransactions + ' </b><br>';
-    outputPortals.innerHTML += '<br><b>' + data.data.payload.transactions.byType[7].numTransactions + ' </b><br>';
-    outputScreenshots.innerHTML += '<br><b>' + data.data.payload.transactions.byType[8].numTransactions + ' </b><br>';
+    outputWallets.innerHTML += '<br><b>' +  data.data.payload.wallets.toLocaleString("en-US") + ' </b><br>';
+    outputCrystallizeNFT.innerHTML += '<br><b>' + data.data.payload.transactions.byType[4].numTransactions.toLocaleString("en-US") + ' </b><br>';
+    outputMeltNFT.innerHTML += '<br><b>' + data.data.payload.transactions.byType[5].numTransactions.toLocaleString("en-US") + ' </b><br>';
+    outputPictures.innerHTML += '<br><b>' + data.data.payload.transactions.byType[6].numTransactions.toLocaleString("en-US") + ' </b><br>';
+    outputPortals.innerHTML += '<br><b>' + data.data.payload.transactions.byType[7].numTransactions.toLocaleString("en-US") + ' </b><br>';
+    outputScreenshots.innerHTML += '<br><b>' + data.data.payload.transactions.byType[8].numTransactions.toLocaleString("en-US") + ' </b><br>';
 
     //console.log("wallets = " +data.data.payload.wallets);
     //console.log("number of transactions = " +data.data.payload.numTransactions);
@@ -46,10 +49,10 @@
     //console.log("issued supply = " +data.data.payload.issuedSupply);
     //console.log("transactions = " +data.data.payload.transactions.byType[0].numTransactions + ' Action Type = ' +data.data.payload.transactions.byType[0].actionType);
     
-    data.data.payload.transactions.byType.forEach(function (val) {
+    //data.data.payload.transactions.byType.forEach(function (val) {
       //outputTransactionTypes.innerHTML +='<br><b>' +  val.numTransactions +'    ' + val.actionType + '</b><br>';
-      console.log("transactions = " + val.numTransactions + " Action Type =" + val.actionType);
-    });
+      //console.log("transactions = " + val.numTransactions + " Action Type =" + val.actionType);
+    //});
     //console.log("claimable = " +data.data.payload.claimable);
     //console.log("Number of Linked Accounts = " +data.data.payload.numLinkedAccounts);
   }).catch(function (error) {
